793e230c2730a2acf0e33b2b0b75ff7b45b819d4
PatxaSec <106649207+PatxaSec@users.noreply.github.com>
2023-10-21 02:24:16 +0200

    myBSPWM

--- Config/bin/ethernet_status.sh
+++ Config/bin/ethernet_status.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -n "$(ifconfig eth0 2>/dev/null | grep 'inet ')" ]; then
-    echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep 'inet ' | awk '{print $2}')"
+    echo "%{F#710193} %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep 'inet ' | awk '{print $2}')"
 elif [ -n "$(ifconfig ens33 2>/dev/null | grep 'inet ')" ]; then
-    echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep 'inet ' | awk '{print $2}')"
+    echo "%{F#710193} %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep 'inet ' | awk '{print $2}')"
 elif [ -n "$(ifconfig wlan0 2>/dev/null | grep 'inet ')" ]; then
-    echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig wlan0 | grep 'inet ' | awk '{print $2}')"
+    echo "%{F#710193} %{F#ffffff}$(/usr/sbin/ifconfig wlan0 | grep 'inet ' | awk '{print $2}')"
 else
-    echo "Disconnected"
+    echo "%{F#e51d0b}󰌙 Disconnected"
 fi
