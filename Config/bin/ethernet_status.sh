570ac83d2efcb9d4303d998d16742bf92413d347
PatxaSec <106649207+PatxaSec@users.noreply.github.com>
2023-10-29 02:44:18 +0200

    Actualizar ethernet_status.sh

--- Config/bin/ethernet_status.sh
+++ Config/bin/ethernet_status.sh
@@ -7,5 +7,5 @@ elif [ -n "$(ifconfig ens33 2>/dev/null | grep 'inet ')" ]; then
 elif [ -n "$(ifconfig wlan0 2>/dev/null | grep 'inet ')" ]; then
     echo "%{F#710193} %{F#ffffff}$(/usr/sbin/ifconfig wlan0 | grep 'inet ' | awk '{print $2}')"
 else
-    echo "%{F#e51d0b}󰌙 Disconnected"
+    echo "%{F#e51d0b}"
 fi
